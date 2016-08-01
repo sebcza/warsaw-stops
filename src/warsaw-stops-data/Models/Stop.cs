7c213ffd446dd0abb7710d00d81f62ba6e208e4a
Sebastian Czarnecki <sczarnecki@pgs-soft.com>
2016-08-01 15:58:36 +0200

    ztm-data with bugs. To repair. Version .NET Core is a problem.

---
+++ src/warsaw-stops-data/Models/Stop.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace warsaw_stops_data.Models
+{
+    public class Stop
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Number { get; set; }
+        public string Direction { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public double Lat { get; set; }
+        public double Long { get; set; }
+    }
+}
---
+++ src/warsaw-stops-data/WarsawStopsContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using warsaw_stops_data.Models;
+
+namespace warsaw_stops_data
+{
+    public class WarsawStopsContext : DbContext
+    {
+        public DbSet<Stop> Stops { get; set; }
+    }
+}
